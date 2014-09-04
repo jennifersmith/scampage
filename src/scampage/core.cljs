ba1a9e102301e295ddfaa23920681c3647a4c2ce
jennifersmith <jennifer@home>
2014-09-04 18:14:09 +1000

    Suspcted bug creating behaviour

--- src/scampage/core.cljs
+++ src/scampage/core.cljs
@@ -4,10 +4,17 @@
 
 (println "Hello world!")
 
+(defn switch-to-scene [name]
+  (.enterScene js/Crafty name))
+
+
+;;(make-scene "Game" game-scene game-scene-uninit)
+
 (defn start-game []
   (.init js/Crafty 480 320)
   (.background js/Crafty "#6698FF")
-;;  (switch-to-scene "Intro")
+  ;;(switch-to-scene "Intro")
   )
 
 (.addEventListener js/window "load" start-game)
+
